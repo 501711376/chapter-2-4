c8e68412743342b2c937997d6bacfb4319096e9e
501711376 <31967587+501711376@users.noreply.github.com>
2017-09-28 08:35:36 -0400

    fix issue

--- time.py
+++ time.py
@@ -1,5 +1,5 @@
 timenow=float(input("what time is it now 0-24"))
 hourswait=float(input("How many hours are you waiting?"))
-alarmtime=hourswait+timenow
-print("your alarm will go off at:"+str(alarmtime))                   
+alarmtime=(hourswait+timenow)%24
+print("your alarm will go off at:"+str(alarmtime))                  
         
