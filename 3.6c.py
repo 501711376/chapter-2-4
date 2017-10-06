27a490479fa4935175f80d61f7402016db4bfbf6
501711376 <31967587+501711376@users.noreply.github.com>
2017-10-06 08:39:37 -0400

    chap 3 homework

---
+++ 3.6c.py
@@ -0,0 +1,7 @@
+
+import turtle
+wn = turtle.Screen()
+alex=turtle.Turtle()
+for i in range(6):
+    alex.forward(50)
+    alex.left(60)
---
+++ 36d.py
@@ -0,0 +1,7 @@
+
+import turtle
+wn = turtle.Screen()
+alex=turtle.Turtle()
+for i in range(8):
+    alex.forward(100)
+    alex.left(45)
